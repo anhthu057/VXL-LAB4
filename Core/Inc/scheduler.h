77828457150e50bae4fe3da9e3a61c9e4c82ac31
LAPTOP-L9MC2346\AnhThu <nathuw1010@gmail.com>
2025-11-14 04:30:13 +0700

    Update

---
+++ Core/Inc/scheduler.h
@@ -0,0 +1,39 @@
+/*
+ * scheduler.h
+ *
+ *  Created on: Nov 13, 2025
+ *      Author: AnhThu
+ */
+
+#ifndef INC_SCHEDULER_H_
+#define INC_SCHEDULER_H_
+
+#include <stdint.h>
+
+typedef struct {
+	void (*pTask)(void);
+	uint32_t	Delay;
+	uint32_t	Period;
+	uint8_t		RunMe;
+
+	uint32_t	TaskID;
+} sTask;
+
+#define SCH_MAX_TASKS	40
+
+void SCH_Init(void);
+
+//settimer
+void SCH_Add_Task ( void (*pFunction)(),
+					uint32_t DELAY,
+					uint32_t PERIOD);
+
+//timerrun
+void SCH_Update(void);
+
+//flag
+void SCH_Dispatch_Tasks(void);
+
+uint8_t SCH_Delete_Task(uint32_t TaskID);
+
+#endif /* INC_SCHEDULER_H_ */
